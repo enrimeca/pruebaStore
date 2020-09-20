cc7521c99ee88f4c5b9bbd9b0cfa15f5ad9cb9df
Enrique Mejia <enriquemejiacano@gmail.com>
2020-09-20 12:41:57 -0500

    snapshot

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -8,7 +9,14 @@ describe('<Footer />', () => {
   test('Render del componente Footer', () => {
     expect(footer.length).toEqual(1);
   });
-  test('Render del titulo', () => {
+  test('Render del título', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
