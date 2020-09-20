026e2f5c86f622fb0b3cab15cbba8984322e752e
Enrique Mejia <enriquemejiacano@gmail.com>
2020-09-20 16:58:35 -0500

    actualizando repositorio

--- src/__test__/utils/getData.test.js
+++ src/__test__/utils/getData.test.js
@@ -7,11 +7,12 @@ describe('Fetch API', () => {
 
   test('Llamar una API y retornar datos', () => {
     fetch.mockResponseOnce(JSON.stringify({ data: '12345' }));
+
     getData('https://google.com')
       .then((response) => {
         expect(response.data).toEqual('12345');
       });
-
-      expect(fetch.mock.calls[0].[0]).toEqual('https://google.com');
+    expect(fetch.mock.calls[0][0]).toEqual('https://google.com');
   });
+
 });
